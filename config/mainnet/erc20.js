17589cda421ba38eb917177ec0d97db6fa95809e
AtomsAreCool <64755060+AtomsAreCool@users.noreply.github.com>
2020-05-23 11:42:58 +0200

    Update erc20.js

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -1,23 +1,18 @@
 export default {
   swap: {
-    address: '0x14a52cf6B4F68431bd5D9524E4fcD6F41ce4ADe9',
-    decimals: 18,
-    fullName: 'SWAP',
+    address: '0xb91c2a2b953d72f3ef890490669a0a41b0add5f7',
+    decimals: 8,
+    fullName: 'BEFX',
   },
   snm: {
-    address: '0x983f6d60db79ea8ca4eb9968c6aff8cfa04b3c63',
+    address: '0x515669d308f887fd83a471c7764f5d084886d34d',
     decimals: 18,
-    fullName: 'snm',
+    fullName: 'MUXE',
   },
   eurs: {
-    address: '0xdb25f211ab05b1c97d595516f45794528a807ad8',
+    address: '0x998FFE1E43fAcffb941dc337dD0468d52bA5b48A',
     decimals: 2,
-    fullName: 'EURS',
-  },
-  xeur: {
-    address: '0xC31C61cf55fB5E646684AD8E8517793ec9A22c5e',
-    decimals: 0,
-    fullName: 'XEUR',
+    fullName: 'IDRT',
   },
   usdt: {
     address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
