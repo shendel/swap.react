ef55356a62dde4835ff32ec9344ffbd7fcc0bceb
shendel <im_shendel@inbox.ru>
2020-05-23 14:12:51 +0300

    example config for enable custom erc20 tokens

--- externalConfigs/swap.dex-belifex.com.js
+++ externalConfigs/swap.dex-belifex.com.js
@@ -1,3 +1,27 @@
+// Uncomment this for enable custom erc20 tokens
+/*
+window.widgetERC20Tokens = {
+  // Symbol of ERC20 token in lowerCase
+  proxima: {
+    // Address of ERC20 contract
+    address: '0xc060b26e60698e91a6acc84051a26b32e38dd1a4',
+    // Count of decimals after dot
+    decimals: 18,
+    // Display name in wallet (By default - its symbol of ERC20, but can be other userfriendy text)
+    fullName: 'Proxima',
+    // Icon of currency (image)
+    icon: 'https://growup.wpmix.net/wp-content/uploads/2016/10/favicon.png',
+    // Background color of icon
+    iconBgColor: '#ccc',
+  },
+  usdt: {
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    decimals: 6,
+    fullName: 'Usdt'
+  },
+}
+*/
+
 // Uncomment this for enable comisions
 /*
 window.widgetERC20Comisions = {
