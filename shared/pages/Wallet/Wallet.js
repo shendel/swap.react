078943db13d93554a825295c26f221980b53921d
shendel <im_shendel@inbox.ru>
2020-10-06 22:17:12 +0300

    withdraw global - fix balance condition

--- shared/pages/Wallet/Wallet.js
+++ shared/pages/Wallet/Wallet.js
@@ -364,7 +364,7 @@ export default class Wallet extends Component {
       // @ToDo - В будущем нужно убрать проверку только по типу монеты.
       // Старую проверку оставил, чтобы у старых пользователей не вывалились скрытые кошельки
 
-      return (!hiddenCoinsList.includes(currency) && !hiddenCoinsList.includes(`${currency}:${address}`)) || balance > 0
+      return (!hiddenCoinsList.includes(currency) && !hiddenCoinsList.includes(`${currency}:${address}`)) && balance > 0
     })
 
     const { currency, address } = tableRows[0];
